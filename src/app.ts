ab1948c1e3468b4c4caad791dbde366191e6babb
jasleen <jasleenkaurkalsi03@gmail.com>
2025-04-27 14:00:37 -0500

    added the workflow for bandit and codeql.

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.use("/api/v1/tasks", taskRoutes);
 
-
+// response
 app.use((err: Error, req: Request, res: Response, next: NextFunction): void => {
 	console.error(err);
 	res.status(500).json({ error: "Internal Server Error" });
